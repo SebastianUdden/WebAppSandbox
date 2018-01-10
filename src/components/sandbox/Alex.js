337d045527cc07fc90427bfe207abea43e117520
Alexander Arana <alexander.arana@eurostep.com>
2018-01-10 15:01:14 +0100

    Alex: till !

--- src/components/sandbox/Alex.js
+++ src/components/sandbox/Alex.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default class Alex extends React.Component {
   render() {
+    let hello = "hej";
     return (
       <div>
-        <h1>Alex</h1>
-        <p>Fria tester av JSX inom denna component.</p>
+        <h1>{hello} Alex</h1>
+        <p>Fria tester av JSX inom denna component!</p>
       </div>
     );
   }
