a81d60b0342aea9b884c7f0b162edf8624fa09d2
Sebastian Uddén <sebastian.udden@eurostep.com>
2018-01-10 14:05:50 +0100

    Hackathon welcome

--- src/components/sandbox/Viktor.js
+++ src/components/sandbox/Viktor.js
@@ -8,7 +8,7 @@ export default class Viktor extends React.Component {
     }
     return (
       <div>
-        <h1>Viktor</h1>
+        <h1>Välkommen Viktor</h1>
         <p >Fria tester av JSX och React inom denna component.</p>
         <h4>Välkomna till denna upplaga av <span style={hackathon}>Hackaton!</span></h4>
         <img width="700px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png" />
