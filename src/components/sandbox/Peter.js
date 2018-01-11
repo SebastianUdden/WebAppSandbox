da6728ec7ca023cf2b3012ae807b26db7243838f
Sebastian Uddén <sebastian.udden@eurostep.com>
2018-01-11 16:36:33 +0100

    Test of Peter.js

--- src/components/sandbox/Peter.js
+++ src/components/sandbox/Peter.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default class Peter extends React.Component {
   render() {
+    let red = {
+      color: "Red"
+    };
+    let h1 = {
+      fontSize: "36px"
+    };
     return (
       <div>
-        <h1>Välkommen Peter</h1>
+        <h1 style={{...h1,...red}}>Välkommen Peter</h1>
         <p>Fria tester av JSX inom denna component.</p>
       </div>
     );
