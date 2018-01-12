e3ac492b8138457729745ed830ca019388da8ffa
Sebastian Uddén <sebastian.udden@eurostep.com>
2018-01-12 16:34:25 +0100

    Added new style

--- src/components/sandbox/Peter.js
+++ src/components/sandbox/Peter.js
@@ -3,18 +3,31 @@ import React from 'react';
 export default class Peter extends React.Component {
   render() {
     let hackathon = {
-      color: 'Red',
-      fontSize: '200%'
+      color: 'OrangeRed',
+      fontSize: '130%'
     };
+    let bolder = {
+      fontWeight: '700',
+      color: 'MediumSpringGreen',
+    };
+    let info = {
+      time: '10-22',
+      food: 'Pizza',
+      beer: 'Frivilligt',
+    }
     return (
       <div>
         <h1>Välkommen Peter,</h1>
         <h4>till denna upplaga av <span style={hackathon}>Hackaton!</span></h4>
-        <p>Från 10 till 22 kör vi igång.</p>
+        <ul>
+          <li><span style={bolder}>Tid:</span> {info.time}</li>
+          <li><span style={bolder}>Mat:</span> {info.food}</li>
+          <li><span style={bolder}>Öl:</span> {info.beer}</li>
+        </ul>
         <p>Fria tester av JSX och React inom denna component...</p>
         <hr />
-        <h4>Följ med på en oförglömglig resa till kodförståelse med årets första <span style={hackathon}>Hackaton</span></h4>
-        <img width="700px" src="http://www.simpleimageresizer.com/_uploads/photos/c824bb96/hackathonViktor_99.jpg" />
+        <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
+        <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
       </div>
     );
   }
