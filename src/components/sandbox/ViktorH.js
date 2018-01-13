2dc0c095f2f951ac7744690551e3f7d95657f9ba
Viktor Högberg <viktor.hogberg@eurostep.com>
2018-01-13 12:09:12 +0100

    knapp och input definierade

--- src/components/sandbox/ViktorH.js
+++ src/components/sandbox/ViktorH.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export default class ViktorH extends React.Component {
+  
+  handleClick(name)  {
+    alert(`Hej ${name}`)
+  }
+  
   render() {
+
     let hackathon = {
       color: 'OrangeRed',
       fontSize: '130%'
@@ -15,6 +21,15 @@ export default class ViktorH extends React.Component {
       food: 'Pizza',
       beer: 'Frivilligt',
     }
+    let buttonStyle = {
+      width: '300px',
+      backgroundColor: 'red', 
+    }
+    let input = {
+      width: '300px',
+      marginTop: '5px',
+      marginBottom: '5px',
+    }
     return (
       <div>
         <h1>Välkommen Viktor Högberg,</h1>
@@ -27,6 +42,8 @@ export default class ViktorH extends React.Component {
         <p>Fria tester av JSX och React inom denna component...</p>
         <hr />
         <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
+        <input type="text" className="form form-control" style={input} /*onChange={() => this*/ />
+        <button style={buttonStyle} className="btn btn-success" onClick={() => this.handleClick("Viktor")}>test</button>
         <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
       </div>
     );
