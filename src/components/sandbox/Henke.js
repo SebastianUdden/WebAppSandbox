1e9dff66bbe7cbaaf804c8b84381eed7b6c6aed9
Henrik Fridström <henrik.fridstrom@eurostep.com>
2018-01-13 12:40:13 +0100

    Henrik

--- src/components/sandbox/Henke.js
+++ src/components/sandbox/Henke.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default class Henke extends React.Component {
   render() {
+    let hello = "Hej";
     return (
       <div>
-        <h1>Välkommen Henke</h1>
+        <h1>{hello} Henke</h1>
+        document.getElementById('root')
         <p>Fria tester av JSX inom denna component.</p>
       </div>
     );
