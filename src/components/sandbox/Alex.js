809e2b88dfd3f78d8a2373fb1eedb71dccb013bb
Ichurep <arana.alexander@gmail.com>
2018-01-13 19:48:06 +0100

    Alex:ddas

--- src/components/sandbox/Alex.js
+++ src/components/sandbox/Alex.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export default class Alex extends React.Component {
-  handleClick(name) {
-    alert(`Hej ${name}`);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      name: "Alex"
+    };
   }
+
+  handleClick() {
+    alert(`Hej ${this._name.value}`);
+    // alert(`Hej ${this.hej.type}`);
+  }
+
+  handleCount() {
+    this.setState({
+      count: this.state.count +1,
+    });
+
+  }
+
   render() {
     let input = {
       width: '40vw',
@@ -39,8 +57,11 @@ export default class Alex extends React.Component {
         <p>Fria tester av JSX och React inom denna component...</p>
         <hr />
         <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
-        <input style={input} type="text" className="form-control"/>
-        <button style={button} className="btn btn-success" onClick={() => this.handleClick("Alex")}>Click!</button>
+        <input style={input} type="text" ref={input => this._name = input} className="form-control" />
+        <button style={button} className="btn btn-success" onClick={() => this.handleClick()}>Click!</button>
+        <button style={button} className="btn btn-info" onClick={() => this.handleCount()}>+1</button>
+        <h2>{this.state.count}</h2>
+        <h2>{this.state.name}</h2>
         <hr />
         <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
       </div>
