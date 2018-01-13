ff78ee849f9e479974a546bbbb67743ce9becf60
Henrik Fridström <henrik.fridstrom@eurostep.com>
2018-01-13 16:51:19 +0100

    Henke

---
+++ src/components/sandbox/Henke.1.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default class Henke extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  handleClick() {
+    alert(`Hej ${this._name.value}`);
+  }
+  handleCount() {
+    this.setState({
+      count: this.state.count + 1
+    }) 
+  }
+  render() {
+    let input = {
+      width: '40vw',
+      marginTop: '5px',
+      marginBottom: '5px',
+    };
+    let hackathon = {
+      color: 'OrangeRed',
+      fontSize: '130%',
+    };
+    let bolder = {
+      fontWeight: '700',
+      color: 'MediumSpringGreen',
+    };
+    let info = {
+      time: '10-22',
+      food: 'Pizza',
+      beer: 'Frivilligt',
+    };
+    let button = {
+      width: '20vw',
+      backgroundColor: 'red',
+    };
+    let count = {
+      width: '20vw',
+      backgroundColor: 'red',
+    };
+    return (
+      <div>
+        <h1>Välkommen Henke,</h1>
+        <h4>till denna upplaga av <span style={hackathon}>Hackaton!</span></h4>
+        <ul>
+          <li><span style={bolder}>Tid:</span> {info.time}</li>
+          <li><span style={bolder}>Mat:</span> {info.food}</li>
+          <li><span style={bolder}>Öl:</span> {info.beer}</li>
+        </ul>
+        <p>Fria tester av JSX och React inom denna component...</p>
+        <hr />
+        <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
+        <input style={input} type="text" ref={input => this._name = input} className="form-control"/>
+        <button style={button}  className="btn btn-success" onClick={() => this.handleClick()}>Click!</button>
+        <button style={button}  className="btn btn-success" onClick={() => this.handleCount()}>+1!</button>
+        <h2>{this.state.count}</h2>
+        <hr />
+        <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
+      </div>
+    );
+  }
+}
--- src/components/sandbox/ViktorH.js
+++ src/components/sandbox/ViktorH.js
@@ -2,9 +2,39 @@ import React from 'react';
 import SendToTeamsButton from './tools/SendToTeamsButton';
 import GenerateList from './tools/List';
 
+
 export default class ViktorH extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listEntries: [
+        { property: "tid", value: "10-22" },
+        { property: "mat", value: "pizza" },
+        { property: "öl", value: "frivilligt" },
+      ]
+    };
+  }
 
+  SaveToList() {
+    const currentListEntries = this.state.listEntries;
+    if (this.property.value !== "" && this.value.value !== "" && this.property.value && this.value.value && !currentListEntries.some(x => x.property === this.property.value)) {
+      currentListEntries.push({ property: this.property.value, value: this.value.value });
+      console.log(currentListEntries);
+      this.setState({
+        listEntries: currentListEntries
+      });
+      document.getElementById("listError").hidden = true
+    }
+    else if(currentListEntries.some(x => x.property === this.property.value)) {
+      document.getElementById("listError").innerText = "Same property already found"
+      document.getElementById("listError").hidden = false
+    }
+    else {
+      document.getElementById("listError").innerText = "Value or property missing"
+      document.getElementById("listError").hidden = false
+    }
 
+  }
   render() {
     let hackathon = {
       color: 'OrangeRed',
@@ -22,26 +52,30 @@ export default class ViktorH extends React.Component {
       marginTop: '5px',
       marginBottom: '5px',
     };
-
-    let listEntries = [
-      {property: "tid", value: "10-22"},
-      {property: "mat", value: "pizza"},
-      {property: "öl", value: "frivilligt"},
-    ];
+    let button = {
+      width: '20vw',
+      backgroundColor: 'blue',
+    };
+    let warning = {
+      color: 'Red',
+      fontSize: '18px'
+    };
 
     return (
       <div>
         <h1>Välkommen Viktor Högberg,</h1>
         <h4>till denna upplaga av <span style={hackathon}>Hackaton!</span></h4>
-        <GenerateList myList={listEntries}/>
-        <p>Fria tester av JSX och React inom denna component...</p>
-        <hr />
-        <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
-        <input id="inputForm" type="text" className="form form-control" style={inputStyle} ref={input => this.message = input} />
-        
-
-        <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
-      </div>
+        <input id="inputProperty" type="text" className="form form-control" style={inputStyle} placeholder="Property" ref={input => this.property = input} />
+        <input id="inputValue" type="text" className="form form-control" style={inputStyle} placeholder="Value" ref={input => this.value = input} />
+        <button id="saveListEntry" style={button} className="btn btn-success" onClick={() => this.SaveToList()}>Save</button>
+        <GenerateList myList={this.state.listEntries} />
+        <p id = "listError" style={warning} hidden> Warning: Error fix </p>
+      <p>Fria tester av JSX och React inom denna component...</p>
+      <hr />
+      <h4>Följ med på en oförglömglig resa till <span style={hackathon}>kodförståelse</span> med årets första <span style={hackathon}>hackaton</span></h4>
+      <input id="inputForm" type="text" className="form form-control" style={inputStyle} ref={input => this.message = input} />
+      <img width="1000px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTX4HgyT3L9Ul8Dm1Jicsrg2ZO9uPHIxyuGXlTO5cXjps42UfYw" />
+      </div >
     );
   }
 }
